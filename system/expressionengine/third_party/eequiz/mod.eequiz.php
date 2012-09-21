0d99d5675a38c6858e855d00f9b2b57755a5c8b0
rm <robert@breeze.local>
2012-09-21 12:12:01 -0600

    EE Quiz mod.eequiz.php modifications for faster score-card reporting

--- system/expressionengine/third_party/eequiz/mod.eequiz.php
+++ system/expressionengine/third_party/eequiz/mod.eequiz.php
@@ -27,6 +27,203 @@ class Eequiz {
         $this->EE =& get_instance();
     }
 
+	/*
+	 *	Added by Robert McCann
+	 *	4-ee guys need to rethink their shit
+	 *
+	 *	I'm using the poormans hack to provide quiz grouping
+	 *	A prefix can be used on the title of the quiz, and be provided to the score card functions
+	 *	This should be implemented into the database at some point
+	 *	
+	 *	This one is probably a little faster, but not as accurate as it relies on the cached_quizzes table
+	 *	Also, this doesn't show if the users has completed all questions required (is quiz complete?)
+	 *	
+	 *	{quiz_id}
+	 *	{quiz_title}
+	 *	{grade_percent}
+	 *	{date_completed}
+	 *	{passing}
+	 *	
+	 */
+	function score_card() {
+		$tagdata = $this->EE->TMPL->tagdata;
+
+		$prefix = $this->EE->db->escape_str( $this->EE->TMPL->fetch_param('prefix') );
+		$active_member_id = $this->_get_active_member_id($quiz);
+
+		//mmm chunky SQL
+		$scores = $this->EE->db->query(
+			"SELECT exp_eequiz_quizzes.quiz_id AS quiz_id,
+				exp_eequiz_quizzes.title AS quiz_title,
+				ROUND(scores.percent*100) AS grade_percent,
+				quiz_progress.last_answer_time AS last_answer_time,
+				exp_eequiz_quizzes.passing_grade AS passing_grade, 
+				IF (scores.percent*100 > exp_eequiz_quizzes.passing_grade, 'TRUE', 'FALSE') AS passing,
+				DATEDIFF(NOW(), FROM_UNIXTIME(last_answer_time)) AS days_since_last_answer
+			FROM exp_eequiz_quizzes
+			LEFT JOIN (
+				SELECT quiz_id, MAX(time) AS last_answer_time FROM exp_eequiz_mappings
+				INNER JOIN exp_eequiz_progress ON exp_eequiz_progress.mapping_id=exp_eequiz_mappings.mapping_id
+				WHERE member_id = $active_member_id
+				GROUP BY exp_eequiz_mappings.quiz_id
+			) AS quiz_progress ON exp_eequiz_quizzes.quiz_id=quiz_progress.quiz_id
+			LEFT JOIN (
+				SELECT * FROM exp_eequiz_cached_scores WHERE member_id=$active_member_id
+			) AS scores ON scores.quiz_id=exp_eequiz_quizzes.quiz_id
+			WHERE exp_eequiz_quizzes.title LIKE '$prefix%' AND exp_eequiz_quizzes.disabled=0
+			ORDER by exp_eequiz_quizzes.title"
+		);
+
+		$this->return_data = $this->EE->TMPL->parse_variables( $tagdata, $scores->result_array() );
+		return $this->return_data;
+	}
+	
+	function score_card2($prefix=NULL) {
+		if(is_null($prefix)){
+			$prefix = $this->EE->db->escape_str( $this->EE->TMPL->fetch_param('prefix') );
+		}
+		$active_member_id = $this->_get_active_member_id($quiz);
+
+		$tagdata = $this->EE->TMPL->tagdata;
+
+		//Extra Chunky SQL...
+		//I don't really trust the "cached_answers" table, so I'm pulling the full results from progress
+		//I'd Imagine this could be a little less crazy (fewer sub queries anyone?), but it gets the job done accurately with all the data you'd want
+		//Also, I'd reason that the .0019 seconds this takes on my server is better than the 4+ second page render time the normal quiz tag takes
+		$scores = $this->EE->db->query(
+			"SELECT 
+				exp_eequiz_quizzes.quiz_id AS quiz_id,
+				exp_eequiz_quizzes.title AS quiz_title,
+				user_grade,
+				passing_grade,
+				num_correct,
+				num_required,
+				last_answer_time,
+				days_since_last_answer,
+				attempted_all_required
+			FROM 
+			exp_eequiz_quizzes
+			LEFT JOIN (
+				SELECT 
+					exp_eequiz_mappings.quiz_id,
+					ROUND((SUM(exp_eequiz_questions.weight)/min_passing_score)*100) AS user_grade,
+					COUNT(*) AS num_correct,
+					num_required,
+					MAX(exp_eequiz_progress.time) AS last_answer_time,
+					DATEDIFF(NOW(), FROM_UNIXTIME(exp_eequiz_progress.time)) AS days_since_last_answer,
+					IF (quiz_progress.num_attempted >= quiz_progress.num_required, 'TRUE', 'FALSE') AS attempted_all_required
+				FROM exp_eequiz_progress
+				INNER JOIN exp_eequiz_mappings ON exp_eequiz_mappings.mapping_id = exp_eequiz_progress.mapping_id
+				INNER JOIN exp_eequiz_questions ON exp_eequiz_questions.question_id = exp_eequiz_mappings.question_id
+				INNER JOIN exp_eequiz_quizzes ON exp_eequiz_quizzes.quiz_id = exp_eequiz_mappings.quiz_id
+				INNER JOIN (
+					SELECT quiz_id, SUM(weight) as min_passing_score
+					FROM exp_eequiz_mappings
+					INNER JOIN exp_eequiz_questions ON exp_eequiz_questions.question_id = exp_eequiz_mappings.question_id
+					WHERE optional = 0
+					GROUP BY quiz_id
+				) AS quiz_required_scores ON quiz_required_scores.quiz_id = exp_eequiz_mappings.quiz_id
+				INNER JOIN (
+					SELECT exp_eequiz_mappings.quiz_id, COUNT(*) AS num_attempted, num_required
+					FROM exp_eequiz_progress
+					INNER JOIN exp_eequiz_mappings ON exp_eequiz_mappings.mapping_id = exp_eequiz_progress.mapping_id
+					INNER JOIN exp_eequiz_questions ON exp_eequiz_questions.question_id = exp_eequiz_mappings.question_id
+					INNER JOIN (
+						SELECT quiz_id, COUNT(*) AS num_required
+						FROM exp_eequiz_questions
+						INNER JOIN exp_eequiz_mappings ON exp_eequiz_mappings.question_id = exp_eequiz_questions.question_id
+						WHERE optional=0
+						GROUP BY quiz_id
+					) AS required ON required.quiz_id = exp_eequiz_mappings.quiz_id
+					WHERE optional=0 AND member_id=$active_member_id
+					GROUP BY exp_eequiz_mappings.quiz_id
+				) AS quiz_progress ON quiz_progress.quiz_id = exp_eequiz_mappings.quiz_id
+				WHERE 
+					member_id=$active_member_id
+					AND user_answer=answer
+				GROUP BY exp_eequiz_mappings.quiz_id
+			) AS user_quiz_results ON user_quiz_results.quiz_id = exp_eequiz_quizzes.quiz_id
+			WHERE exp_eequiz_quizzes.title LIKE '$prefix%'"
+		);
+
+		$this->return_data = $this->EE->TMPL->parse_variables( $tagdata, $scores->result_array() );
+		return $this->return_data;
+	}
+	
+	//Returns the number of quizzes that a user is passing in a group
+	//If I trusted the cached_scores table, this would be simpler.
+	//The group is defined as the template tag parameter: prefix
+	//Default behavior is current user
+	function number_passing_in_group($prefix=NULL, $member_id=NULL) {
+		if(is_null($prefix)){
+			$prefix = $this->EE->db->escape_str( $this->EE->TMPL->fetch_param('prefix') );
+		}
+
+		if(is_null($member_id)) { $member_id = $this->_get_active_member_id($quiz); };
+
+		$active_member_id = $member_id;
+
+		//mmm chunky SQL
+		$number_passing_in_group = $this->EE->db->query(
+			"SELECT COUNT(*) AS number_passing
+			FROM exp_eequiz_quizzes
+			INNER JOIN 
+			(
+			SELECT exp_eequiz_mappings.quiz_id, ROUND((SUM(weight) / min_passing_score)*100) AS user_grade
+			FROM exp_eequiz_progress
+			INNER JOIN exp_eequiz_mappings ON exp_eequiz_mappings.mapping_id = exp_eequiz_progress.mapping_id
+			INNER JOIN exp_eequiz_questions ON exp_eequiz_questions.question_id = exp_eequiz_mappings.question_id
+			RIGHT JOIN (
+				SELECT quiz_id, SUM(weight) as min_passing_score
+				FROM exp_eequiz_mappings
+				INNER JOIN exp_eequiz_questions ON exp_eequiz_questions.question_id=exp_eequiz_mappings.question_id
+				WHERE optional = 0
+				GROUP BY quiz_id
+			) AS quiz_required_scores ON quiz_required_scores.quiz_id = exp_eequiz_mappings.quiz_id
+			WHERE 
+				user_answer=answer 
+				AND member_id = $active_member_id
+			GROUP BY exp_eequiz_mappings.quiz_id
+			) AS user_grades
+			ON user_grades.quiz_id=exp_eequiz_quizzes.quiz_id
+			WHERE title LIKE '$prefix%' AND user_grade>=passing_grade"
+		);
+		
+		return $number_passing_in_group->row('number_passing');
+	}
+
+	//Returns the number of quizzes in a "group"
+	//The group is defined as the template tag parameter: prefix
+	function number_in_group($prefix=NULL, $member_id=NULL) {
+		$prefix = $this->EE->db->escape_str( $this->EE->TMPL->fetch_param('prefix') );
+
+		//mmm chunky SQL
+		$number_in_group = $this->EE->db->query(
+			"SELECT COUNT(*) AS number_in_group
+			FROM exp_eequiz_quizzes
+			WHERE title LIKE '$prefix%'"
+		);
+		
+		return $number_in_group->row('number_in_group');
+	}
+
+	function passing_all_in_group($prefix=NULL, $member_id=NULL){
+		$prefix = $this->EE->db->escape_str( $this->EE->TMPL->fetch_param('prefix') );
+
+		if(is_null($member_id)) { $member_id = $this->_get_active_member_id($quiz); };
+		
+		$active_member_id = $member_id;
+
+		if($this->number_passing_in_group($prefix, $active_member_id)==$this->number_in_group($prefix)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/*
+	 *	END of section added by Robert McCann
+	 */
 
 	// -------------------------------------
     //  quizzes
