c15edb3ed6fc647dfed429d160071f45d5f9f817
rm <robert@breeze.local>
2012-10-10 10:40:27 -0600

    Clean up and fix materials model

--- system/expressionengine/third_party/workreports/libraries/axapta/models/materials.php
+++ system/expressionengine/third_party/workreports/libraries/axapta/models/materials.php
@@ -6,12 +6,11 @@ class materials extends axapta {
 	protected $item_name           = 'PRICEDISCTABLE.RTDITEMNAME';
 	protected $name                = 'CONFIGTABLE.NAME';
 	protected $unit                = 'PRICEDISCTABLE.UNITID';
-	protected $price_type          = 'PRICEDISCTABLE.RTDPRICETYPE';
 	protected $ammount             = 'PRICEDISCTABLE.AMOUNT';
 	protected $contract_id         = 'RTDCONTRACT.CONTRACTID';
 	protected $contract_valid      = 'RTDCONTRACT.VALID';
-	protected $contract_valid_from = 'RTDCONTRACT.VALIDFROM';
-	protected $contract_valit_to   = 'RTDCONTRACT.VALIDTO';
+	protected $contract_valid_from = 'CONVERT(DATE, RTDCONTRACT.VALIDFROM)';
+	protected $contract_valit_to   = 'CONVERT(DATE, RTDCONTRACT.VALIDTO)';
 	protected $film_indicator      = 'INVENTTABLE.RTDFILMIND';
 
 	function __construct($conn){
@@ -29,18 +28,18 @@ class materials extends axapta {
 	function get_remote($options = NULL) {
 		//set some defaults
 		if( !is_array($options) ){
-			$options = array('valid_contract' => '1');
+			//$options = array('contract_valid' => '1');
 			$options = array('film_indicator' => '1');
 		} else {
-			$options['valid_contract'] = '1';
-			$options['film_indicator'] = '1';
+			//$options['contract_valid'] = '1';
+			//$options['film_indicator'] = '1';
 		}
 
 		$query = $this->build_SELECT();
 
 		$query .= 'FROM PROJTABLE'.NL;
 		$query .= 'JOIN SALESTABLE      ON PROJTABLE.PROJID = SALESTABLE.projID AND PROJTABLE.DATAAREAID = SALESTABLE.DATAAREAID'.NL;
-		$query .= 'JOIN PRICEDISCTABLE  ON LTRIM(RTRIM(SALESTABLE.PriceGroupID)) = LTRIM(RTRIM(PRICEDISCTABLE.AccountRelation)) AND PROJTABLE.DATAAREAID = PRICEDISCTABLE.DATAAREAID'.NL;
+		$query .= 'JOIN PRICEDISCTABLE  ON LTRIM(SALESTABLE.PriceGroupID) = LTRIM(PRICEDISCTABLE.AccountRelation) AND PROJTABLE.DATAAREAID = PRICEDISCTABLE.DATAAREAID'.NL;
 		$query .= 'JOIN RTDCONTRACT     ON RTDCONTRACT.CONTRACTID = LTRIM(RTRIM(SALESTABLE.PriceGroupID)) AND PROJTABLE.DATAAREAID = RTDCONTRACT.DATAAREAID'.NL;
 		$query .= 'JOIN INVENTTABLE     ON PRICEDISCTABLE.ITEMRELATION = INVENTTABLE.ITEMID AND PROJTABLE.DATAAREAID = INVENTTABLE.DATAAREAID'.NL;
 		$query .= 'JOIN INVENTDIM       ON INVENTDIM.INVENTDIMID = PRICEDISCTABLE.INVENTDIMID AND PROJTABLE.DATAAREAID = INVENTDIM.DATAAREAID'.NL;
@@ -61,6 +60,7 @@ class materials extends axapta {
 
 		$this->bind_option_values( $materials, $options );
 		$materials->setFetchMode(PDO::FETCH_NAMED);
+		$materials->execute();
 
 		return $this->fix_padding( $materials->fetchAll() );
 	}
