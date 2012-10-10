a06957412c441413c36fe955480e691f7b761e01
rm <robert@breeze.local>
2012-10-10 10:42:06 -0600

    New Contract_items model

---
+++ system/expressionengine/third_party/workreports/libraries/axapta/models/contract_items.php
@@ -0,0 +1,67 @@
+<?php
+class contract_items extends axapta {
+	protected $id                  = 'LTRIM(PRICEDISCTABLE.INVENTDIMID)';
+	protected $item_id             = 'LTRIM(INVENTTABLE.ITEMID)';
+	protected $name                = 'PRICEDISCTABLE.RTDITEMNAME';
+	protected $unit                = 'PRICEDISCTABLE.UNITID';
+	protected $price_type          = 'PRICEDISCTABLE.RTDPRICETYPE';
+	protected $price               = 'PRICEDISCTABLE.AMOUNT';
+	protected $currency            = 'PRICEDISCTABLE.CURRENCY';
+
+	protected $contract_id         = 'RTDCONTRACT.CONTRACTID';
+	//protected $contract_valid      = 'RTDCONTRACT.VALID';
+	//protected $contract_valid_from = 'RTDCONTRACT.VALIDFROM';
+	//protected $contract_valit_to   = 'RTDCONTRACT.VALIDTO';
+
+	protected $film_indicator      = 'INVENTTABLE.RTDFILMIND';
+
+	function __construct($conn){
+		$this->conn =& $conn;
+		$this->properties = $this->get_properties();
+	}
+	
+	/*
+	 *  Materials
+	 *
+	 *	Options: 
+	 *	Defaults: 
+	 *
+	 */
+	function get_remote($options = NULL) {
+		//set some defaults
+		if( !is_array($options) ){
+			//$options = array('contract_valid' => '1');
+			//$options = array('film_indicator' => '1');
+		} else {
+			//$options['contract_valid'] = '1';
+			//$options['film_indicator'] = '1';
+		}
+
+		$query = $this->build_SELECT();
+
+		$query .= 'FROM PRICEDISCTABLE'.NL;
+		$query .= 'JOIN RTDCONTRACT     ON RTDCONTRACT.CONTRACTID = LTRIM(PRICEDISCTABLE.ACCOUNTRELATION) AND RTDCONTRACT.DATAAREAID = PRICEDISCTABLE.DATAAREAID'.NL;
+		$query .= 'JOIN INVENTTABLE     ON PRICEDISCTABLE.ITEMRELATION = INVENTTABLE.ITEMID AND INVENTTABLE.DATAAREAID = PRICEDISCTABLE.DATAAREAID'.NL;
+		$query .= 'JOIN INVENTDIM       ON INVENTDIM.INVENTDIMID = PRICEDISCTABLE.INVENTDIMID AND INVENTDIM.DATAAREAID = PRICEDISCTABLE.DATAAREAID'.NL;
+		$query .= 'JOIN CONFIGTABLE     ON CONFIGTABLE.CONFIGID = INVENTDIM.CONFIGID AND CONFIGTABLE.ITEMID = INVENTTABLE.ITEMID AND CONFIGTABLE.DATAAREAID = PRICEDISCTABLE.DATAAREAID'.NL;
+
+		$query .= $this->build_WHERE( $options );
+
+		$query .= NL.'ORDER BY PRICEDISCTABLE.RTDITEMNAME';
+
+		if( isset($_GET['output']) && $_GET['output'] == 'debug' ){
+			echo '<pre>'.$query.'</pre>';
+			echo '<pre>';
+			print_r($options);
+			echo '</pre>';
+		}
+
+		$contract_items = $this->conn->prepare($query);
+
+		$this->bind_option_values( $contract_items, $options );
+		$contract_items->setFetchMode(PDO::FETCH_NAMED);
+		$contract_items->execute();
+
+		return $this->fix_padding( $contract_items->fetchAll() );
+	}
+}
