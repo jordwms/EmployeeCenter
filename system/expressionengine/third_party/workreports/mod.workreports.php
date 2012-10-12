3be7138155e8580f8c1f230d195fa588e725188d
rm <robert@breeze.local>
2012-10-12 11:51:04 -0600

    REST API return proper headers for json output

--- system/expressionengine/third_party/workreports/mod.workreports.php
+++ system/expressionengine/third_party/workreports/mod.workreports.php
@@ -130,6 +130,10 @@ class Workreports {
 				switch ($output) {
 					default:
 					case 'json':
+						header('Cache-Control: no-cache, must-revalidate');
+						header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+						header('Content-type: application/json');
+
 						echo json_encode($return_data);
 					break;
 					
