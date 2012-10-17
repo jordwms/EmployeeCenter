6c9f2dceb2d4e76d9cfb34238629d916a8b93fce
Jordan <profjord@gmail.com>
2012-10-17 16:56:59 -0600

    Tweaking print( )

--- system/expressionengine/third_party/workreports/mod.workreports.php
+++ system/expressionengine/third_party/workreports/mod.workreports.php
@@ -7,11 +7,16 @@ class Workreports {
 		$this->EE =& get_instance();
 		$this->EE->load->library('axapta/axapta');
 		$this->EE->load->library('tcpdf/tcpdf');
+		// $this->EE->load->library('tcpdf/config/lang/eng');
 		$this->EE->load->library('mysql');
 		$this->EE->config->set_item('compress_output', FALSE); 
 	}
 
-	function example() {
+	function wrPrint() {
+
+		// $this->EE->load->template('print');
+		require_once('libraries/tcpdf/config/lang/eng.php');
+
 		// create new PDF document
 		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
@@ -66,120 +71,7 @@ class Workreports {
 		 */
 
 		// define some HTML content with style
-		$html = '
-		<!-- EXAMPLE OF CSS STYLE -->
-		<style>
-		    h1 {
-		        color: navy;
-		        font-family: times;
-		        font-size: 24pt;
-		        text-decoration: underline;
-		    }
-		    p.first {
-		        color: #003300;
-		        font-family: helvetica;
-		        font-size: 12pt;
-		    }
-		    p.first span {
-		        color: #006600;
-		        font-style: italic;
-		    }
-		    p#second {
-		        color: rgb(00,63,127);
-		        font-family: times;
-		        font-size: 12pt;
-		        text-align: justify;
-		    }
-		    p#second > span {
-		        background-color: #FFFFAA;
-		    }
-		    table.first {
-		        color: #003300;
-		        font-family: helvetica;
-		        font-size: 8pt;
-		        border-left: 3px solid red;
-		        border-right: 3px solid #FF00FF;
-		        border-top: 3px solid green;
-		        border-bottom: 3px solid blue;
-		        background-color: #ccffcc;
-		    }
-		    td {
-		        border: 2px solid blue;
-		        background-color: #ffffee;
-		    }
-		    td.second {
-		        border: 2px dashed green;
-		    }
-		    div.test {
-		        color: #CC0000;
-		        background-color: #FFFF66;
-		        font-family: helvetica;
-		        font-size: 10pt;
-		        border-style: solid solid solid solid;
-		        border-width: 2px 2px 2px 2px;
-		        border-color: green #FF00FF blue red;
-		        text-align: center;
-		    }
-		</style>
-
-		<h1 class="title">Example of <i style="color:#990000">XHTML + CSS</i></h1>
-
-		<p class="first">Example of paragraph with class selector. <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed imperdiet lectus. Phasellus quis velit velit, non condimentum quam. Sed neque urna, ultrices ac volutpat vel, laoreet vitae augue. Sed vel velit erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras eget velit nulla, eu sagittis elit. Nunc ac arcu est, in lobortis tellus. Praesent condimentum rhoncus sodales. In hac habitasse platea dictumst. Proin porta eros pharetra enim tincidunt dignissim nec vel dolor. Cras sapien elit, ornare ac dignissim eu, ultricies ac eros. Maecenas augue magna, ultrices a congue in, mollis eu nulla. Nunc venenatis massa at est eleifend faucibus. Vivamus sed risus lectus, nec interdum nunc.</span></p>
-
-		<p id="second">Example of paragraph with ID selector. <span>Fusce et felis vitae diam lobortis sollicitudin. Aenean tincidunt accumsan nisi, id vehicula quam laoreet elementum. Phasellus egestas interdum erat, et viverra ipsum ultricies ac. Praesent sagittis augue at augue volutpat eleifend. Cras nec orci neque. Mauris bibendum posuere blandit. Donec feugiat mollis dui sit amet pellentesque. Sed a enim justo. Donec tincidunt, nisl eget elementum aliquam, odio ipsum ultrices quam, eu porttitor ligula urna at lorem. Donec varius, eros et convallis laoreet, ligula tellus consequat felis, ut ornare metus tellus sodales velit. Duis sed diam ante. Ut rutrum malesuada massa, vitae consectetur ipsum rhoncus sed. Suspendisse potenti. Pellentesque a congue massa.</span></p>
-
-		<div class="test">example of DIV with border and fill.<br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed imperdiet lectus.</div>
-
-		<br />
-
-		<table class="first" cellpadding="4" cellspacing="6">
-		 <tr>
-		  <td width="30" align="center"><b>No.</b></td>
-		  <td width="140" align="center" bgcolor="#FFFF00"><b>XXXX</b></td>
-		  <td width="140" align="center"><b>XXXX</b></td>
-		  <td width="80" align="center"> <b>XXXX</b></td>
-		  <td width="80" align="center"><b>XXXX</b></td>
-		  <td width="45" align="center"><b>XXXX</b></td>
-		 </tr>
-		 <tr>
-		  <td width="30" align="center">1.</td>
-		  <td width="140" rowspan="6" class="second">XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX</td>
-		  <td width="140">XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td width="80">XXXX</td>
-		  <td align="center" width="45">XXXX<br />XXXX</td>
-		 </tr>
-		 <tr>
-		  <td width="30" align="center" rowspan="3">2.</td>
-		  <td width="140" rowspan="3">XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td align="center" width="45">XXXX<br />XXXX</td>
-		 </tr>
-		 <tr>
-		  <td width="80">XXXX<br />XXXX<br />XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td align="center" width="45">XXXX<br />XXXX</td>
-		 </tr>
-		 <tr>
-		  <td width="80" rowspan="2" >XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td align="center" width="45">XXXX<br />XXXX</td>
-		 </tr>
-		 <tr>
-		  <td width="30" align="center">3.</td>
-		  <td width="140">XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td align="center" width="45">XXXX<br />XXXX</td>
-		 </tr>
-		 <tr bgcolor="#FFFF80">
-		  <td width="30" align="center">4.</td>
-		  <td width="140" bgcolor="#00CC00" color="#FFFF00">XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td width="80">XXXX<br />XXXX</td>
-		  <td align="center" width="45">XXXX<br />XXXX</td>
-		 </tr>
-		</table>';
+		$html = $this->wrDetails();
 
 		// output the HTML content
 		$pdf->writeHTML($html, true, false, true, false, '');
@@ -701,6 +593,7 @@ class Workreports {
 		if ( $employee = $this->EE->axapta->employee->get_remote( array('email' => $this->EE->session->userdata('email')) ) ){
 			$tagdata = $this->EE->TMPL->tagdata;
 
+
 			$this->EE->db->select('
 						project_id,
 						sales_id,
@@ -750,8 +643,6 @@ class Workreports {
 		}
 	}
 
-
-	// Remake of wrDetails(), but it comes from MySQL instead of Axapta
 	function wrDetails() {
 		// if ( ($employee = $this->EE->axapta->employee()) ) {
 			$employee = $this->EE->axapta->employee->get_remote( array('email' => $this->EE->session->userdata('email')) );
@@ -885,7 +776,6 @@ class Workreports {
 			return $this->return_data;
 		// }
 	}
-
 
 	function contract_items(){
 		if( $contract_id = $this->EE->TMPL->fetch_param('contract_id') ){
