dc25175a908cad984478781afdbbfebb47abe766
Jordan <profjord@gmail.com>
2012-10-22 15:14:38 -0600

    - Adjusting the PDF variables to better fit it's task. - Removing comments.

--- system/expressionengine/third_party/workreports/mod.workreports.php
+++ system/expressionengine/third_party/workreports/mod.workreports.php
@@ -23,7 +23,7 @@ class Workreports {
 		$pdf->SetCreator('Applus RTD');
 		$pdf->SetAuthor('Applus RTD');
 		$pdf->SetTitle('Test Work Report');
-		$pdf->SetSubject('TCPDF Tutorial');
+		$pdf->SetSubject('Applus RTD Invoice');
 		$pdf->SetKeywords('TCPDF, PDF, test, report');
 
 		// set default header data
@@ -54,24 +54,10 @@ class Workreports {
 		//set some language-dependent strings
 		$pdf->setLanguageArray($l);
 
-		// ---------------------------------------------------------
-
 		// set font
 		$pdf->SetFont('helvetica', '', 10);
 
-		// add a page
 		$pdf->AddPage();
-
-		/* NOTE:
-		 * *********************************************************
-		 * You can load external XHTML using :
-		 *
-		 * $html = file_get_contents('/path/to/your/file.html');
-		 *
-		 * External CSS files will be automatically loaded.
-		 * Sometimes you need to fix the path of the external CSS.
-		 * *********************************************************
-		 */
 
 		// define some HTML content with style
 		$html = $this->wrDetails();
@@ -80,63 +66,6 @@ class Workreports {
 
 		// output the HTML content
 		$pdf->writeHTML($html);
-
-		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-		// *******************************************************************
-		// HTML TIPS & TRICKS
-		// *******************************************************************
-
-		// REMOVE CELL PADDING
-		//
-		// $pdf->SetCellPadding(0);
-		// 
-		// This is used to remove any additional vertical space inside a 
-		// single cell of text.
-
-		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-		// REMOVE TAG TOP AND BOTTOM MARGINS
-		//
-		// $tagvs = array('p' => array(0 => array('h' => 0, 'n' => 0), 1 => array('h' => 0, 'n' => 0)));
-		// $pdf->setHtmlVSpace($tagvs);
-		// 
-		// Since the CSS margin command is not yet implemented on TCPDF, you
-		// need to set the spacing of block tags using the following method.
-
-		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-		// SET LINE HEIGHT
-		//
-		// $pdf->setCellHeightRatio(1.25);
-		// 
-		// You can use the following method to fine tune the line height
-		// (the number is a percentage relative to font height).
-
-		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-		// CHANGE THE PIXEL CONVERSION RATIO
-		//
-		// $pdf->setImageScale(0.47);
-		// 
-		// This is used to adjust the conversion ratio between pixels and 
-		// document units. Increase the value to get smaller objects.
-		// Since you are using pixel unit, this method is important to set the
-		// right zoom factor.
-		// 
-		// Suppose that you want to print a web page larger 1024 pixels to 
-		// fill all the available page width.
-		// An A4 page is larger 210mm equivalent to 8.268 inches, if you 
-		// subtract 13mm (0.512") of margins for each side, the remaining 
-		// space is 184mm (7.244 inches).
-		// The default resolution for a PDF document is 300 DPI (dots per 
-		// inch), so you have 7.244 * 300 = 2173.2 dots (this is the maximum 
-		// number of points you can print at 300 DPI for the given width).
-		// The conversion ratio is approximatively 1024 / 2173.2 = 0.47 px/dots
-		// If the web page is larger 1280 pixels, on the same A4 page the 
-		// conversion ratio to use is 1280 / 2173.2 = 0.59 pixels/dots
-
-		// *******************************************************************
 
 		// reset pointer to the last page
 		$pdf->lastPage();
